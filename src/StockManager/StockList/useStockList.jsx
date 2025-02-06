8f773eca4276c4bd9b733b86580860f9d7bbbc03
carlos0550 <carlospelinski03@gmail.com>
2025-02-06 18:41:30 -0300

    Contiene toda la logica de visualizacion y busqueda de stock

---
+++ src/StockManager/StockList/useStockList.jsx
@@ -0,0 +1,183 @@
+import { Button, Modal, notification, Popover } from 'antd'
+import React, { useEffect, useRef, useState } from 'react'
+import { apis } from '../../../apis'
+import { SettingOutlined } from '@ant-design/icons'
+import { useAppContext } from '../../Context/AppContext'
+
+function useStockList() {
+
+  const {
+    products, setProducts,
+    getInitialProducts
+  } = useAppContext()
+  const [openModalId, setOpenModalId] = useState(null)
+  const [openImagesModalId, setOpenImagesModalId] = useState(null)
+  const [searchText, setSearchText] = useState("")
+  const [searching, setSearching] = useState(false)
+
+  const alreadySearched = useRef(false)
+  useEffect(()=>{
+    if(searchText.trim() === "" && !alreadySearched.current){
+      alreadySearched.current = true
+      getInitialProducts()
+    } 
+  },[getInitialProducts, searchText])
+
+  const normalizedText = (text) => {
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()
+  }
+  const localeSearch = () => {
+    const productFound = products.find(pr =>
+      normalizedText(pr.product_name).includes(normalizedText(searchText))
+    );
+  
+    if (productFound) {
+      return setProducts([productFound]);
+    }
+  
+    return null;
+  };
+  
+  const handleSearch = async () => {
+      alreadySearched.current = false
+    try {
+      setSearching(true);
+      
+      const foundProduct = localeSearch();
+  
+      if (!foundProduct) {
+        const url = apis.products
+        const newUrl = new URL(`${url}/search-product/${searchText}`)
+        const response = await fetch(newUrl)
+        const responseData = await response.json()
+        if(!response.ok) return notification.error({
+          message: responseData.message,
+          showProgress: true,
+          pauseOnHover: false,
+          duration: 3
+        })
+
+        setProducts(responseData.products);
+        return;
+      }
+  
+    } catch (error) {
+      console.log("Error en la búsqueda:", error);
+    } finally {
+      setSearching(false);
+    }
+  };
+  
+
+  const stockListColumns = [
+    {
+      title: "Producto",
+      render: (_, record) => {
+        return (
+          <strong>{record.product_name}</strong>
+        )
+      }
+    }, {
+      title: "Descripción",
+      render: (_, record) => {
+        const isOpen = openModalId === record.product_id
+
+        return (
+          <>
+            <Button onClick={() => setOpenModalId(record.product_id)}>Ver descripción</Button>
+            <Modal
+              key={record.product_id}
+              open={isOpen}
+              title={record.product_name}
+              onCancel={() => setOpenModalId(null)}
+              footer={[
+                <Button
+                  key={record.product_id}
+                  onClick={() => setOpenModalId(null)}
+                  danger
+                >
+                  Cerrar
+                </Button>
+              ]}
+            >
+              <div dangerouslySetInnerHTML={{ __html: record.product_description }}></div>
+            </Modal>
+          </>
+        )
+      }
+    }, {
+      title: "Precio y stock",
+      render: (_, record) => {
+        const stock = record.product_stock;
+        const price = parseFloat(record.product_price).toLocaleString("es-AR", { style: 'currency', currency: "ARS" })
+        return (
+          <div>
+            <p><strong>Precio: </strong>{price} c/u</p>
+            <p><strong>Stock: </strong>{stock}</p>
+          </div>
+        )
+      }
+    }, {
+      title: "Imágenes",
+      render: (_, record) => {
+        if (!record.images || record.images.length === 0) {
+          return "No hay imágenes";
+        }
+
+        const imagesArray = record.images.map(img => {
+          const fullUrl = `${apis.products}${img}`;
+          return fullUrl.replace("/products/", "/");
+        });
+
+        const isOpen = openImagesModalId === record.product_id
+        return (
+          <div >
+            <Button onClick={() => setOpenImagesModalId(record.product_id)}>Ver imagenes</Button>
+
+            <Modal
+              key={record.product_id}
+              open={isOpen}
+              title={record.product_name}
+              onCancel={() => setOpenImagesModalId(null)}
+              footer={[
+                <Button
+                  key={record.product_id}
+                  onClick={() => setOpenImagesModalId(null)}
+                  danger
+                >
+                  Cerrar
+                </Button>
+              ]}
+            >
+              <div className='minified-stock-list-images-container'>
+                {imagesArray.map((image, index) => (
+                  <picture key={index} className='minified-stock-list-images'>
+                    <img src={image} alt={`Imagen ${index + 1}`} />
+                  </picture>
+                ))}
+              </div>
+            </Modal>
+          </div>
+        );
+      }
+    }, {
+      render: (_, record) => (
+        <Popover>
+          <Button icon={<SettingOutlined />} variant='outlined' />
+        </Popover>
+      )
+    }
+
+  ]
+
+  useEffect(()=>{
+    console.log(searchText)
+  },[searchText])
+  return {
+    stockListColumns,
+    handleSearch,
+    setSearchText
+  }
+}
+
+export default useStockList
